6b4aaa52c07d0d5f6f34d757040efd5ccc3bd8be
Zera <zera-ora@tuta.io>
2020-06-28 20:58:03 +0100

    Added login and register functionality

--- docs/login-system/backend.js
+++ docs/login-system/backend.js
@@ -1,3 +1,55 @@
-Parse.serverURL = 'https://parseapi.back4app.com';
-Parse.initialize('di6IUv0xY3edmDR6v6OOBeGOO22t0d1YKunKhVn4', 'duUFCRrT7hRAWCXUyeh3p8aOqksu6JkuhMX2VkVM');
+function initializeParse() {
+  Parse.serverURL = 'https://parseapi.back4app.com';
+  Parse.initialize('yuRcZi2r0wpaFn2zTIkepiCnLq2Di6fsUOxJsfh2', 'duUFCRrT7hRAWCXUyeh3p8aOqksu6JkuhMX2VkVM');
+}
 
+
+function messenger(element) {
+  return (type, message) => {
+    document.getElementById(element).innerHTML = '<span class="' + type + '">' + message + '</span>';
+  }
+}
+
+
+function registerUser() {
+  const username = document.getElementById('registerUsername').value;
+  const password = document.getElementById('registerPassword').value;
+  let message = messenger('registerMessage');
+
+  if (username.trim() === '' || password.trim() === '') {
+    message('error', 'A username and password is required.');
+    return;
+  } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password)) {
+    message('error', 'The password must contain 8 characters, an uppercase letter, a lowercase letter and a number.');
+    return;
+  }
+
+  const user = new Parse.User();
+  user.set('username', username);
+  user.set('password', password);
+
+  user.signUp().then(user => {
+    message('success', 'Created account successfully.');
+  }).catch(err => {
+    message('error', 'An error occurred when creating the account: ' + err);
+  });
+}
+
+function loginUser() {
+  const username = document.getElementById('loginUsername').value;
+  const password = document.getElementById('loginPassword').value;
+  let message = messenger('loginMessage');
+
+  Parse.User.logIn(username, password).then(user => {
+    message('success', 'Logged in successfully.');
+  }).catch(err => {
+    message('error', 'An error occurred when logging in.');
+  });
+}
+
+
+
+initializeParse();
+
+document.getElementById('register').addEventListener('click', registerUser);
+document.getElementById('login').addEventListener('click', loginUser);
